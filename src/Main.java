01a6e4e1d6c871d9a9c003ceabc7ae1c37bc15dc
unknown <joshua31041@gmail.com>
2017-09-30 15:06:32 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int x,y;
+        double c,d;
+        x=in.nextInt();
+        y=in.nextInt();
+        c=x/2.54;
+        d=y/0.454;
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
